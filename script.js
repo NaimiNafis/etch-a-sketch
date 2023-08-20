737e1ac29816be9c75b15f37e34cfe18b7754f90
MeeSup <126528516+MeeSup@users.noreply.github.com>
2023-08-20 09:33:31 +0900

    Change var squaresNum to gridSize Fix color change stay when hover out

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const grid = document.querySelector('#grid');
 grid.classList.add('#grid');
 
-const squareNum = 16;
+const gridSize = 32;
 
 //16x16 row and column
 
 let addSquares = function (){
-    for (let i = 0; i <= squareNum; i++){
+    for (let i = 0; i <= gridSize; i++){
         const row = document.createElement('div');
         row.classList.add('row')
         grid.appendChild(row);
-        for (let j = 0; j <= squareNum; j++){
+        for (let j = 0; j <= gridSize; j++){
             const column = document.createElement('div');
             column.classList.add('column')
             row.appendChild(column);
@@ -18,5 +18,14 @@ let addSquares = function (){
     }
 }
 
-addSquares(squareNum);
+addSquares(gridSize);
+
+//Change color when hover
+
+const squares = document.querySelectorAll('.column');
+squares.forEach((square) => {
+    square.addEventListener("mouseover", () => {
+        square.classList.add("colorChange");
+      }); 
+})
 
