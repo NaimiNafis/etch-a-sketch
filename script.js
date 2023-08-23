4617a4d9cca1dc35c81c0cfedc7468248b04aa90
MeeSup <126528516+MeeSup@users.noreply.github.com>
2023-08-23 19:30:57 +0900

    Refactor erase, clear, black, and random functions Refactor dark shade function Add removeEventListener function and Apply it on changeColor and applyDarkShade function

--- script.js
+++ script.js
@@ -1,153 +1,117 @@
-//Default 16x16 row and column
+// Default 16x16 row and column
 let gridSize = 16;
-function addSquares(){
-    for (let i = 0; i < gridSize; i++){
-        const row = document.createElement('div');
-        row.classList.add('row')
-        grid.appendChild(row);
-        for (let j = 0; j < gridSize; j++){
-            const column = document.createElement('div');
-            column.classList.add('column')
-            row.appendChild(column);
-        }
-    }
-}
 
-addSquares();
-
-//Clear grid 
+// Selectors for buttons
 const gridClear = document.getElementById('clearBtn');
-gridClear.addEventListener('click', clearGrid);
-
-function clearGrid(){
-    const squares = document.querySelectorAll('.column');
-    squares.forEach((square) => {
-        square.style.backgroundColor = 'white';
-    }); 
-}
-
-//Change color when dragged
 const randomColorButton = document.getElementById('randomBtn');
 const blackColorButton = document.getElementById('blackBtn');
-
-randomColorButton.addEventListener('click', changeRandomColor);
-blackColorButton.addEventListener('click', changeBlackColor);
-let isDragging = false;
-
-function getRandomColor() {
-    let r = Math.floor(Math.random() * 256);
-    let g = Math.floor(Math.random() * 256);
-    let b = Math.floor(Math.random() * 256);
-    return `rgb(${r}, ${g}, ${b})`;
-}
-
-function changeRandomColor() {
-    const squares = document.querySelectorAll('.column');
-    squares.forEach((square) => {
-        square.addEventListener('mousedown', () => {
-            isDragging = true;
-            square.style.backgroundColor = getRandomColor();
-        })
-        square.addEventListener('mouseenter', () => {
-            if (isDragging === true){
-                square.style.backgroundColor = getRandomColor();
-            }
-        })
-        square.addEventListener('mouseup', () => {
-            isDragging = false;
-        })
-    });
-}
-
-function changeBlackColor() {
-    const squares = document.querySelectorAll('.column');
-    squares.forEach((square) => {
-        square.addEventListener('mousedown', () => {
-            isDragging = true;
-            square.style.backgroundColor = 'black';
-        })
-        square.addEventListener('mouseenter', () => {
-            if (isDragging === true){
-                square.style.backgroundColor = 'black';
-            }
-        })
-        square.addEventListener('mouseup', () => {
-            isDragging = false;
-        })
-    });
-}
-
-//Erase to white
 const gridEraser = document.getElementById('eraserBtn');
-gridEraser.addEventListener('click', eraser);
+const darkShade = document.getElementById('darkShadeBtn');
+const sizeChange = document.getElementById('sizeBtn');
 
-function eraser() {
+// Utility functions
+function createSquare(className = '') {
+    const square = document.createElement('div');
+    square.classList.add(className);
+    return square;
+}
+
+function applyColor(square, color) {
+    square.style.backgroundColor = color;
+}
+
+function removeEventListeners() {
     const squares = document.querySelectorAll('.column');
-    squares.forEach((square) => {
-        square.addEventListener('mousedown', () => {
-            isDragging = true;
-            square.style.backgroundColor = 'white';
-        })
-        square.addEventListener('mouseenter', () => {
-            if (isDragging === true){
-                square.style.backgroundColor = 'white';
-            }
-        })
-        square.addEventListener('mouseup', () => {
-            isDragging = false;
-        })
+    squares.forEach(square => {
+        const newSquare = square.cloneNode(true);
+        square.parentNode.replaceChild(newSquare, square);
     });
 }
 
-//Shade effect on colored column
-
-const darkShade = document.getElementById('darkShadeBtn');
-darkShade.addEventListener('click', darkenColumn);
-let currentDarkness = 1;
-
-function darkenColumn() {
+// Event listeners for buttons
+gridClear.addEventListener('click', () => {
     const squares = document.querySelectorAll('.column');
-    let currentDarkness = 1;
-    squares.forEach((square) => {
+    squares.forEach(square => applyColor(square, 'white'));
+});
+
+randomColorButton.addEventListener('click', () => {
+    changeColor(getRandomColor());
+});
+
+blackColorButton.addEventListener('click', () => {
+    changeColor('black');
+});
+
+gridEraser.addEventListener('click', () => {
+    changeColor('white');
+});
+
+darkShade.addEventListener('click', () => {
+    applyDarkShade();
+});
+
+sizeChange.addEventListener('click', getUserInput);
+
+// Functions for changing colors
+let isDragging = false;
+function changeColor(color) {
+    removeEventListeners(); 
+    const squares = document.querySelectorAll('.column');
+    squares.forEach(square => {
+        square.addEventListener('mousedown', () => {
+            isDragging = true;
+            applyColor(square, color);
+        });
         square.addEventListener('mouseenter', () => {
-            const newDarkness = Math.max(currentDarkness - 0.1, 0);
-            const currentColor = window.getComputedStyle(square).backgroundColor;
-            const rgb = currentColor.match(/\d+/g);
-            const newColor = `rgb(${Math.floor(rgb[0] * newDarkness)}, ${Math.floor(rgb[1] * newDarkness)}, ${Math.floor(rgb[2] * newDarkness)})`;
-            square.style.backgroundColor = newColor;
+            if (isDragging === true) {
+                applyColor(square, color);
+            }
+        });
+        square.addEventListener('mouseup', () => {
+            isDragging = false;
         });
     });
 }
 
-// function darkenColumn() {
-//     currentDarkness -= 0.1;
-//     if (currentDarkness < 0) {
-//         currentDarkness = 0;
-//     }
-//     const squares = document.querySelectorAll('.column');
-//     squares.forEach((square) => {
-//         const currentColor = window.getComputedStyle(square).backgroundColor;
-//         const rgb = currentColor.match(/\d+/g);
-//         const newColor = `rgb(${rgb[0] * currentDarkness}, ${rgb[1] * currentDarkness}, ${rgb[2] * currentDarkness})`;
-//         //the newColor rgb value should be (0,0,0) or black at the end of 10 iteration
-//         square.addEventListener('mousedown', () => {
-//             isDragging = true;
-//             square.style.backgroundColor = newColor;
-//         })
-//         square.addEventListener('mouseenter', () => {
-//             if (isDragging === true){
-//                 square.style.backgroundColor = newColor;
-//             }
-//         })
-//         square.addEventListener('mouseup', () => {
-//             isDragging = false;
-//         })
-//     });
-// }
+// Apply dark shading effect
+let currentDarkness = 1;
+function applyDarkShade() {
+    removeEventListeners(); 
+    const squares = document.querySelectorAll('.column');
+    let currentDarkness = 1;
+    squares.forEach(square => {
+        square.addEventListener('mousedown', () => {
+            isDragging = true;
+            applyDarkness(square, currentDarkness)
+        });
+        square.addEventListener('mouseenter', () => {
+            if (isDragging === true) {
+                applyDarkness(square, currentDarkness)
+            }
+        });
+        square.addEventListener('mouseup', () => {
+            isDragging = false;
+        });
+    });
+}
 
+function applyDarkness(square, currentDarkness) {
+    const newDarkness = Math.max(currentDarkness - 0.1, 0);
+    const currentColor = window.getComputedStyle(square).backgroundColor;
+    const currentRGB = currentColor.match(/\d+/g);
+    const newColor = `rgb(${Math.floor(currentRGB[0] * newDarkness)}, ${Math.floor(currentRGB[1] * newDarkness)}, ${Math.floor(currentRGB[2] * newDarkness)})`;
+    applyColor(square, newColor);
+}
 
-//Resize column and row when gridSize change
-function resizeColumnsAndRows(){
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+// Resize columns and rows when gridSize changes
+function resizeColumnsAndRows() {
     const containerWidth = 960;
     const columnWidth = containerWidth / gridSize;
     const rowHeight = containerWidth / gridSize;
@@ -155,38 +119,45 @@ function resizeColumnsAndRows(){
     const columns = document.querySelectorAll('.column');
     const rows = document.querySelectorAll('.row');
 
-    columns.forEach((column) => {
+    columns.forEach(column => {
         column.style.width = `${columnWidth}px`;
-    })
+    });
 
-    rows.forEach((row) => {
+    rows.forEach(row => {
         row.style.height = `${rowHeight}px`;
-})
+    });
 }
 
-resizeColumnsAndRows();
+// Create initial grid
+function addSquares() {
+    for (let i = 0; i < gridSize; i++) {
+        const row = createSquare('row');
+        grid.appendChild(row);
+        for (let j = 0; j < gridSize; j++) {
+            const column = createSquare('column');
+            row.appendChild(column);
+        }
+    }
+}
 
-
-//Prompt user to change gridSize using button
-const sizeChange = document.getElementById('sizeBtn');
-sizeChange.addEventListener('click', getUserInput);
-
-function getUserInput(){
-    let userInput = +prompt("Grid Size (Up to 100): ");
+// Handle user input for gridSize change
+function getUserInput() {
+    const userInput = +prompt("Grid Size (Up to 100): ");
     if (userInput !== null) {
         if (userInput <= 100) {
             grid.innerHTML = '';
             gridSize = userInput;
             addSquares(gridSize);
             resizeColumnsAndRows(gridSize);
+        } else {
+            alert("Please enter a value up to 100.");
         }
-        else {
-            alert("Please enter a value up to 100.")
-        }
-    }
-    else {
+    } else {
         alert("No input was provided.");
     }
 }
 
-
+// Initialize the grid
+const grid = document.getElementById('grid');
+addSquares();
+resizeColumnsAndRows();
